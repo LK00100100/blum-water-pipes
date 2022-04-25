616cbe38cfff57e83897f04bf425f58c7996fcf9
L. K <LK00100100@gmail.com>
2022-04-24 23:42:16 -0400

    plot - colored in drain

--- plotter/pipedataplotter.py
+++ plotter/pipedataplotter.py
@@ -20,9 +20,17 @@ class PipeDataPlotter:
 
             x1, y1 = [current.x, next_node.x], [current.height, next_node.height]
 
-            target_color = 'blue' if current.can_drain else 'red'
+            target_color = 'aqua' if current.can_drain else 'pink'
 
             plt.plot(x1, y1, marker='o', color=target_color)
+
+            current = current.next_data
+
+        # color drain nodes
+        current = pipe_data_head
+        while current is not None:
+            if current.is_drain_point:
+                plt.scatter([current.x], [current.height], color='mediumblue', zorder=10)
 
             current = current.next_data
 
